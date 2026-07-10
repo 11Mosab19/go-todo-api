588f9f7f68e69911ac533afb924c386daf16dcb4
11mosab19 <mosabmohammedhome10@gmail.com>
2026-07-11 01:19:17 +0300

    After

--- main.go
+++ main.go
@@ -3,30 +3,76 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
+	"strconv"
 )
 
-func GetData(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(res).Encode(Players)
+func AllPlayerHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Header.Get("Content-Type") != "application/json" {
+		http.Error(res, "error", http.StatusInternalServerError)
+		return
+	}
+	switch req.Method {
+	case http.MethodGet:
+		res.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(res).Encode(Players)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusBadRequest)
+		}
+
+	case http.MethodPost:
+		var NewPlayer PlayerData
+		err := json.NewDecoder(req.Body).Decode(&NewPlayer)
+		if err != nil {
+			http.Error(res, "error", http.StatusBadRequest)
+			return
+		}
+		NewPlayer.Id = len(Players) + 1
+		Players = append(Players, NewPlayer)
+
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusCreated)
+		json.NewEncoder(res).Encode(NewPlayer)
+
+	default:
+		http.Error(res, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 }
 
-func AddPlayer(res http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		http.Error(res, "method not allowed", http.StatusBadRequest)
-		return
-	}
-	var NewPlayer PlayerData
-	err := json.NewDecoder(req.Body).Decode(&NewPlayer)
+func PlayerHandle(res http.ResponseWriter, req *http.Request) {
+	strId := req.PathValue("PlayerID")
+	id, err := strconv.Atoi(strId)
 	if err != nil {
-		http.Error(res, "error", http.StatusBadRequest)
+		http.Error(res, err.Error(), http.StatusBadRequest)
 		return
 	}
-	NewPlayer.Id = len(Players)
-	Players = append(Players, NewPlayer)
+	switch req.Method {
+	case http.MethodGet:
+		res.Header().Set("Content-Type", "application/json")
+		for _, player := range Players {
+			if player.Id == id {
+				err := json.NewEncoder(res).Encode(player)
+				if err != nil {
+					http.Error(res, err.Error(), http.StatusBadRequest)
+					return
+				}
+			}
+		}
+		http.Error(res, "not found", http.StatusNotFound)
+		return
 
-	res.Header().Set("Content-Type", "application/json")
-	res.WriteHeader(http.StatusCreated)
-	json.NewEncoder(res).Encode(NewPlayer)
+	case http.MethodDelete:
+		for i, player := range Players {
+			if player.Id == id {
+				Players = slices.Delete(Players, i, i+1)
+				res.WriteHeader(http.StatusNoContent)
+				return
+			}
+		}
+		http.Error(res, "not found", http.StatusNotFound)
+		return
+	}
 }
 
 type PlayerData struct {
@@ -41,7 +87,8 @@ var Players = []PlayerData{
 }
 
 func main() {
-	http.HandleFunc("/players", GetData)
-	http.HandleFunc("/players/add", AddPlayer)
+	http.HandleFunc("/players", AllPlayerHandler)
+	http.HandleFunc("/players/{PlayerID}", PlayerHandle)
 	http.ListenAndServe(":8080", nil)
+
 }
